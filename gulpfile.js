703e54cc6d7144cf6a6be1855da3a98149896273
Fenvrir <dragonofhokage@gmail.com>
2022-06-11 11:00:53 +0700

    compete mobile 360

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const del = require('del');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const imageMin = require('gulp-imagemin'); 
+const uglify = require('gulp-uglify');
 
 const server = () => {
     browserSync.init({
@@ -39,14 +40,23 @@ const img = () => {
     .pipe(browserSync.stream());
 }
 
+const js = () => {
+    return gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'))
+    .pipe(browserSync.stream());
+}
+
 const watch = () => {
     gulp.watch('./src/*.html', html);
     gulp.watch('./src/sass/**/*.scss', scss);
     gulp.watch('./src/images/**/*', img);
+    gulp.watch('./src/js/*.js', js)
 };
 
 exports.default = gulp.series(
     clear,
+    gulp.series(js),
     gulp.parallel(html, scss, img),
     gulp.parallel(watch, server),
 );
---
+++ src/js/scripts.js
@@ -0,0 +1,36 @@
+'use_strict';
+let burgerToggle = document.querySelector('.header__burger');
+let burgerMenu = document.querySelector('.menu__burger');
+let body = document.querySelector('body');
+
+const toggleClass = () => {
+    if (burgerMenu.classList.contains('disable')) {
+        burgerMenu.classList.add('active');
+        burgerMenu.classList.remove('disable');
+    } else {
+        burgerMenu.classList.add('disable');
+        burgerMenu.classList.remove('active');
+    }
+}
+
+burgerToggle.addEventListener('click', (ev) => {
+    ev.stopPropagation();
+    toggleClass();
+})
+   
+    
+
+burgerMenu.addEventListener('click', (ev) => {
+   
+    toggleClass();
+   
+})
+
+body.addEventListener('click', (ev) => {
+   
+    if (burgerMenu.classList.contains('active')) {
+        burgerMenu.classList.remove('active');
+        burgerMenu.classList.add('disable');
+    }
+    
+})
